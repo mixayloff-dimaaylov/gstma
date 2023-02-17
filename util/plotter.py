d5903cebac5613330e9e35fb7bd5061cd327fba1
mixayloff-dimaaylov <65488726+mixayloff-dimaaylov@users.noreply.github.com>
2023-02-17 20:09:31 +0300

    plotter: Rewrite everything with Pandas
    `ipython-sql` позволяет использовать Pandas в качестве формата
    выгрузок. У DataFrame'ов есть интерфейс словарей, значит можно
    переписать всё под DataFrame'ы без потери совместимости.
    
    Встроенная в Pandas поддержка DateTime облегчает работу со временем,
    сдвигами и перегруппировкой.
    
    Так же возможно реализовать _upsampling_ (интерполяцию на Pandas) [1,
    2], как это сделано для ISMRAWTEC.
    
    Ref: https://www.reddit.com/r/learnpython/comments/vbiflk/how_to_resample_seconds_to_milliseconds_and_then/ [1]
    Ref: https://www.machinelearningplus.com/pandas/pandas-reset-index/ [2]

--- util/plotter.py
+++ util/plotter.py
@@ -31,13 +31,13 @@ get_ipython().system('conda install --yes --prefix {sys.prefix} -c conda-forge c
 import os
 import glob
 import re
-import csv
 import statistics
 import scipy.signal as sc
 import datetime as dt
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 from math import pi
 
@@ -123,30 +123,6 @@ def sigPhi(sigNT, f):
     return 1e16 * 80.8 * pi * sigNT / (C * f)
 
 
-def csvReadAsDict(file):
-    columns = {}
-
-    with open(file) as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=',')
-
-        # read header
-        headers = next(csvreader, None)
-
-        if headers is None:
-            print(f'File {file} is empty.')
-            return columns
-
-        for h in headers:
-            columns[h] = []
-
-        # fill header fields
-        for row in csvreader:
-            for h, v in zip(headers, row):
-                columns[h].append(v)
-
-    return columns
-
-
 # Searches CSV files in ./rawdump/ dir and returns them as list of tuples, for
 # each (satellite, from, to, secondaryfreq)
 def read_csvs():
@@ -155,18 +131,18 @@ def read_csvs():
     files_ismrawtec = glob.glob("./rawdump/rawdata_ismrawtec_*.csv")
     files_satxyz2 = glob.glob("./rawdump/rawdata_satxyz2_*.csv")
 
-    return ({"range": csvReadAsDict(r),
-             "ismrawtec": csvReadAsDict(rt),
-             "satxyz2": csvReadAsDict(xyz)}
+    return ({"range": pd.read_csv(r),
+             "ismrawtec": pd.read_csv(rt),
+             "satxyz2": pd.read_csv(xyz)}
             for r, rt, xyz in zip(files_range, files_ismrawtec, files_satxyz2))
 
 
 def perf_cal(values):
-    values_range = values['range']
-    values_ismrawtec = values['ismrawtec']
-    values_satxyz2 = values['satxyz2']
+    df_range = values['range']
+    df_ismrawtec = values['ismrawtec']
+    df_satxyz2 = values['satxyz2']
 
-    file_a_sat = values_range['sat'][0]
+    file_a_sat = df_range.sat[0]
     file_a_sat_system = re.search('^([A-Z]+)[0-9]+$', file_a_sat).group(1)
 
     if file_a_sat_system == 'GPS':
@@ -189,31 +165,23 @@ def perf_cal(values):
         exit(1)
 
     # RANGE
-    times = np.array([dt.datetime.fromtimestamp(int(ts)/1000) for ts in values_range['time']]).astype(dt.datetime)
-    psr1 = np.array(values_range['psr1']).astype(float)
-    psr2 = np.array(values_range['psr2']).astype(float)
-    psr5 = np.array(values_range['psr5']).astype(float)
-    adr1 = np.array(values_range['adr1']).astype(float)
-    adr2 = np.array(values_range['adr2']).astype(float)
-    adr5 = np.array(values_range['adr5']).astype(float)
-    sat = np.array(values_range['sat']).astype(str)
+    df_range.time = pd.to_datetime(df_range.time, unit='ms', utc=True)
 
     # ISMRAWTEC
-    ism_times_unix, ism_tec, ism_sat = zip(*values_ismrawtec)
-
-    ism_times = np.array([dt.datetime.fromtimestamp(int(ts)/1000) for ts in values_ismrawtec['time']]).astype(dt.datetime)
-    ism_tec = np.array(values_ismrawtec['tec']).astype(float)
-    ism_sat = np.array(values_ismrawtec['sat']).astype(str)
+    df_ismrawtec.time = pd.to_datetime(df_ismrawtec.time, unit='ms', utc=True)
+    df_ismrawtec.set_index('time', inplace=True)
+    df_ismrawtec = df_ismrawtec.resample('20ms')
+    df_ismrawtec = df_ismrawtec.interpolate(method='linear').interpolate(method='ffill')
+    df_ismrawtec.reset_index(inplace=True)
 
     # Расчеты
+    df_range['k12'] = k(df_range.adr1, df_range.adr2, f1, f2, df_range.psr1, df_range.psr2)
+    df_range['k15'] = k(df_range.adr1, df_range.adr5, f1, f5, df_range.psr1, df_range.psr5)
+    df_range['p12'] = df_range.psr2 - df_range.psr1
+    df_range['p15'] = df_range.psr5 - df_range.psr1
 
-    k12 = k(adr1, adr2, f1, f2, psr1, psr2)
-    k15 = k(adr1, adr5, f1, f5, psr1, psr5)
-    p12 = psr2 - psr1
-    p15 = psr5 - psr1
-
-    _DNT12 = statistics.mean(k12)
-    _DNT15 = statistics.mean(k15)
+    _DNT12 = statistics.mean(df_range.k12)
+    _DNT15 = statistics.mean(df_range.k15)
     DNT12 = multiplier(f1, f2) * _DNT12
     DNT15 = multiplier(f1, f5) * _DNT15
 
@@ -222,55 +190,40 @@ def perf_cal(values):
 
     # --- drawings ---
     # fig, ax = plt.subplots()
-    # ax.plot(times, [DNT12] * len(k12), label="DNT12(adr1, adr2, psr1, psr2")
-    # ax.plot(times, [DNT15] * len(k15), label="DNT15(adr1, adr5, psr1, psr5")
-    # ax.plot(times, p12, label="P1-P2")
-    # ax.plot(times, p15, label="P1-P5")
+    # ax.plot(df_range.time, [DNT12] * len(k12), label="DNT12(df_range.adr1, df_range.adr2, df_range.psr1, df_range.psr2")
+    # ax.plot(df_range.time, [DNT15] * len(k15), label="DNT15(df_range.adr1, df_range.adr5, df_range.psr1, df_range.psr5")
+    # ax.plot(df_range.time, p12, label="P1-P2")
+    # ax.plot(df_range.time, p15, label="P1-P5")
     # ax.legend()
 
-    NT12psr = multiplier(f1, f2) * p12 + RDCB_L1L2
-    NT15psr = multiplier(f1, f5) * p15 + RDCB_L1L5
+    df_range['NT12psr'] = multiplier(f1, f2) * df_range.p12 + RDCB_L1L2
+    df_range['NT15psr'] = multiplier(f1, f5) * df_range.p15 + RDCB_L1L5
 
     # print(p12, p15)
 
-    NT12adr_wo_DNT = multiplier(f1, f2) * adr_adr(adr1, adr2, f1, f2)
-    NT15adr_wo_DNT = multiplier(f1, f5) * adr_adr(adr1, adr5, f1, f5)
-    NT12adr = NT12adr_wo_DNT + DNT12 + RDCB_L1L2
-    NT15adr = NT15adr_wo_DNT + DNT15 + RDCB_L1L5
+    df_range['NT12adr_wo_DNT'] = multiplier(f1, f2) * adr_adr(df_range.adr1, df_range.adr2, f1, f2)
+    df_range['NT15adr_wo_DNT'] = multiplier(f1, f5) * adr_adr(df_range.adr1, df_range.adr5, f1, f5)
+    df_range['NT12adr'] = df_range.NT12adr_wo_DNT + DNT12 + RDCB_L1L2
+    df_range['NT15adr'] = df_range.NT15adr_wo_DNT + DNT15 + RDCB_L1L5
 
     # plt.show()
 
-    avgNT12 = avgNT(NT12adr)
-    avgNT15 = avgNT(NT15adr)
+    df_range['avgNT12'] = avgNT(df_range.NT12adr)
+    df_range['avgNT15'] = avgNT(df_range.NT15adr)
 
-    delNT12 = delNT(NT12adr)
-    delNT15 = delNT(NT15adr)
+    df_range['delNT12'] = delNT(df_range.NT12adr)
+    df_range['delNT15'] = delNT(df_range.NT15adr)
 
-    sigNT12 = sigNT(delNT12)
-    sigNT15 = sigNT(delNT15)
+    df_range['sigNT12'] = pd.Series(sigNT(df_range.delNT12)).shift(59, fill_value=0.0)
+    df_range['sigNT15'] = pd.Series(sigNT(df_range.delNT15)).shift(59, fill_value=0.0)
 
-    sigPhi12 = sigPhi(sigNT12, f2)
-    sigPhi15 = sigPhi(sigNT15, f5)
+    df_range['sigPhi12'] = sigPhi(df_range.sigNT12, f2)
+    df_range['sigPhi15'] = sigPhi(df_range.sigNT15, f5)
 
-    # dict_key = file_a_sat + '_' + str(times_unix[0]) + ' ' + str(times_unix[-1])
-    result = {'sat': file_a_sat,
-              'times': times,
-              'ism_times': ism_times,
-              'ism_tec': ism_tec,
-              'NT12psr': NT12psr,
-              'NT15psr': NT15psr,
-              'NT12adr': NT12adr,
-              'NT15adr': NT15adr,
-              'avgNT12': avgNT12,
-              'avgNT15': avgNT15,
-              'delNT12': delNT12,
-              'delNT15': delNT15,
-              'sigNT12': sigNT12,
-              'sigNT15': sigNT15,
-              'sigPhi12': sigPhi12,
-              'sigPhi15': sigPhi15}
+    # For export
+    df_range['ism_tec'] = df_ismrawtec.tec
 
-    return result
+    return df_range
 
 
 def plot_build(sat):
@@ -290,19 +243,19 @@ def plot_build(sat):
 
         gax.plot(xs, ys, label=vname)
 
-    dumpplot(sat['times'],      sat['NT12psr'],  "NT(P1-P2)")
-    dumpplot(sat['times'],      sat['NT15psr'],  "NT(P1-P5)")
-    dumpplot(sat['times'],      sat['NT12adr'],  "NT(adr1 - adr2)")
-    dumpplot(sat['times'],      sat['NT15adr'],  "NT(adr1 - adr5)")
-    dumpplot(sat['ism_times'],  sat['ism_tec'],  "ISMRAWTEC's TEC")
-    dumpplot(sat['times'],      sat['avgNT12'],  "avgNT(12)")
-    dumpplot(sat['times'],      sat['avgNT15'],  "avgNT(15)")
-    dumpplot(sat['times'],      sat['delNT12'],  "delNT(12)")
-    dumpplot(sat['times'],      sat['delNT15'],  "delNT(15)")
-    dumpplot(sat['times'][59:], sat['sigNT12'],  "sigNT(12)")
-    dumpplot(sat['times'][59:], sat['sigNT15'],  "sigNT(15)")
-    dumpplot(sat['times'][59:], sat['sigPhi12'], "sigPhi(12)")
-    dumpplot(sat['times'][59:], sat['sigPhi15'], "sigPhi(15)")
+    dumpplot(sat.time, sat.NT12psr,  "NT(P1-P2)")
+    dumpplot(sat.time, sat.NT15psr,  "NT(P1-P5)")
+    dumpplot(sat.time, sat.NT12adr,  "NT(adr1 - adr2)")
+    dumpplot(sat.time, sat.NT15adr,  "NT(adr1 - adr5)")
+    dumpplot(sat.time, sat.ism_tec,  "ISMRAWTEC's TEC")
+    dumpplot(sat.time, sat.avgNT12,  "avgNT(12)")
+    dumpplot(sat.time, sat.avgNT15,  "avgNT(15)")
+    dumpplot(sat.time, sat.delNT12,  "delNT(12)")
+    dumpplot(sat.time, sat.delNT15,  "delNT(15)")
+    dumpplot(sat.time, sat.sigNT12,  "sigNT(12)")
+    dumpplot(sat.time, sat.sigNT15,  "sigNT(15)")
+    dumpplot(sat.time, sat.sigPhi12, "sigPhi(12)")
+    dumpplot(sat.time, sat.sigPhi15, "sigPhi(15)")
 
     gax.legend()
     plt.title(f"ПЭСы спутника {sat_name}")
@@ -350,6 +303,7 @@ from sqlalchemy import create_engine
 
 
 get_ipython().run_line_magic('load_ext', 'sql')
+get_ipython().run_line_magic('config', 'SqlMagic.autopandas=True')
 
 
 # In[ ]:
@@ -397,19 +351,19 @@ _secondaryfreq = _secondaryfreqw.value
 # In[ ]:
 
 
-get_ipython().run_cell_magic('sql', 'values_range <<', "SELECT\n    time,\n    anyIf(psr, freq = 'L1CA') AS psr1,\n    anyIf(psr, freq = 'L2CA') AS psr2,\n    anyIf(psr, freq = 'L2P') AS psr5,\n    anyIf(adr, freq = 'L1CA') AS adr1,\n    anyIf(adr, freq = 'L2CA') AS adr2,\n    anyIf(adr, freq = 'L2P') AS adr5,\n    any(cno) as cno,\n    sat\nFROM\n    rawdata.range\nWHERE\n    sat=:_sat\n    AND time BETWEEN :_from AND :_to\nGROUP BY\n    time, sat\nORDER BY\n    time ASC\n")
+get_ipython().run_cell_magic('sql', 'df_range <<', "SELECT\n    time,\n    anyIf(psr, freq = 'L1CA') AS psr1,\n    anyIf(psr, freq = 'L2CA') AS psr2,\n    anyIf(psr, freq = 'L2P') AS psr5,\n    anyIf(adr, freq = 'L1CA') AS adr1,\n    anyIf(adr, freq = 'L2CA') AS adr2,\n    anyIf(adr, freq = 'L2P') AS adr5,\n    any(cno) as cno,\n    sat\nFROM\n    rawdata.range\nWHERE\n    sat=:_sat\n    AND time BETWEEN :_from AND :_to\nGROUP BY\n    time, sat\nORDER BY\n    time ASC\n")
 
 
 # In[ ]:
 
 
-get_ipython().run_cell_magic('sql', 'values_ismrawtec <<', 'SELECT\n    time,\n    anyIf(tec, secondaryfreq = :_secondaryfreq) AS tec,\n    sat\nFROM\n    rawdata.ismrawtec\nWHERE\n    sat=:_sat\n    AND time BETWEEN :_from AND :_to\nGROUP BY\n    time,\n    sat\nORDER BY\n    time ASC\n')
+get_ipython().run_cell_magic('sql', 'df_ismrawtec <<', 'SELECT\n    time,\n    anyIf(tec, secondaryfreq = :_secondaryfreq) AS tec,\n    sat\nFROM\n    rawdata.ismrawtec\nWHERE\n    sat=:_sat\n    AND time BETWEEN :_from AND :_to\nGROUP BY\n    time,\n    sat\nORDER BY\n    time ASC\n')
 
 
 # In[ ]:
 
 
-get_ipython().run_cell_magic('sql', 'values_satxyz2 <<', 'SELECT\n    time,\n    elevation,\n    sat\nFROM\n    rawdata.satxyz2\nWHERE\n    sat=:_sat\n    AND time BETWEEN :_from AND :_to\nORDER BY\n    time ASC\n')
+get_ipython().run_cell_magic('sql', 'df_satxyz2 <<', 'SELECT\n    time,\n    elevation,\n    sat\nFROM\n    rawdata.satxyz2\nWHERE\n    sat=:_sat\n    AND time BETWEEN :_from AND :_to\nORDER BY\n    time ASC\n')
 
 
 # ### Модификация исходной программы
@@ -420,9 +374,9 @@ get_ipython().run_cell_magic('sql', 'values_satxyz2 <<', 'SELECT\n    time,\n   
 # Замена для источника данных
 def read_sql():
     return [dict(
-             {"range": values_range.dict(),
-              "ismrawtec": values_ismrawtec.dict(),
-              "satxyz2": values_satxyz2.dict()})]
+             {"range": df_range,
+              "ismrawtec": df_ismrawtec,
+              "satxyz2": df_satxyz2})]
 
 
 # ### Расчеты
@@ -430,7 +384,7 @@ def read_sql():
 # In[ ]:
 
 
-if values_range.DataFrame().empty:
+if df_range.empty:
     print("Выгрузка пуста!")
 else:
     for values in read_sql():
